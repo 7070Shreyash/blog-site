b5598dfd13389babe5644f3065869862918fff83
shreyash <shreyashacharya001@gmail.com>
2024-08-04 03:37:55 +0530

    done

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'https://blog-site-frontend.netlify.app'];
+  const allowedOrigins = ['http://localhost:3000', 'https://blog-site-frontend.netlify.app', 'https://blog-site-frontend-2.netlify.app'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
